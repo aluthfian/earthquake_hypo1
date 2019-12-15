23610c88a356c54c5be8668086abf8a149f0e244
aluthfian <47322232+aluthfian@users.noreply.github.com>
2019-12-15 18:27:29 +0700

    Update polygon_area.m

--- polygon_area.m
+++ polygon_area.m
@@ -8,6 +8,9 @@ file_id=fopen('vertices_bangka.txt','r');
 % koordinat titik-titik pada pantai Pulau Bangka
 % satuan angka adalah meter
 vertices_bangka=transp(fscanf(file_id,'%f %f',[2 Inf]));
+% rubah koordinat universal ke koordinat lokal
+vertices_bangka(:,1)=vertices_bangka(:,1)-min(vertices_bangka(:,1));
+vertices_bangka(:,2)=vertices_bangka(:,2)-min(vertices_bangka(:,2));
 
 % tutup file
 fclose(file_id);
@@ -33,3 +36,12 @@ end
 
 % luas Pulau Bangka dalam kilometer persegi
 area_bangka=0.5*(prod1-prod2);
+
+%% plotting
+patch('Faces',faces_bangka,'Vertices',vertices_bangka./1e+3)
+hold on
+scatter(vertices_bangka(:,1)./1e+3,vertices_bangka(:,2)./1e+3)
+txtplot=cellstr(num2str(transp(1:32)));
+text((vertices_bangka(:,1)./1e+3)+1, (vertices_bangka(:,2)./1e+3)+1, txtplot);
+title({'Pulau Bangka',['luasnya ',num2str(area_bangka,'%.0f'),' km^2.']})
+hold off
