f54864e9bd1468da18f534db3d83a61601c5e7d6
aluthfian <47322232+aluthfian@users.noreply.github.com>
2019-12-20 21:47:51 +0700

    Simplify my code
    Simplified the program using “circshift” function from Matlab: https://www.mathworks.com/help/matlab/ref/circshift.html#bt5nt1s-1_1.

--- polygon_area.m
+++ polygon_area.m
@@ -18,23 +18,9 @@ fclose(file_id);
 % nomor titik pada pantai Pulau Bangka
 faces_bangka=horzcat(1:32,1);
 
-%% perhitungan luas
-prod1=0;
-prod2=0;
-iter=length(vertices_bangka);
-for i=1:iter
-    if i==iter
-        j=1;
-    else
-        j=i+1;
-    end
-    
-    % ubah jarak dari meter ke km
-    prod1=prod1+(vertices_bangka(i,1)*vertices_bangka(j,2)/1e+6); 
-    prod2=prod2+(vertices_bangka(i,2)*vertices_bangka(j,1)/1e+6);
-end
-
-% luas Pulau Bangka dalam kilometer persegi
+%% perhitungan luas Pulau Bangka dalam kilometer persegi
+prod1=1e-6*sum(vertices_bangka(:,1).*circshift(vertices_bangka(:,2),1));
+prod2=1e-6*sum(vertices_bangka(:,2).*circshift(vertices_bangka(:,1),1));
 area_bangka=0.5*(prod1-prod2);
 
 %% plotting
